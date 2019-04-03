7dea45439b3baf37ea342e8517592d5a169fcb82
Ankit <ankit.ime2@gmail.com>
2019-04-03 13:03:52 +0530

    Changes in demo.java

--- demo.java
+++ demo.java
@@ -5,7 +5,7 @@ public class AddTwoNumbers {
 
    public static void main(String[] args) {
         
-      int num1 = 5, num2 = 15, sum;
+      int num1 = 5, num2 = 15, num3 = 10, sum;
       sum = num1 + num2;
 
       System.out.println("Sum of these numbers: "+sum);
